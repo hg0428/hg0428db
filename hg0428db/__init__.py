67558136fa9226cda58602d5f89c558f45b52d23
hg0428 <45984899+hg0428@users.noreply.github.com>
2022-04-19 16:12:06 +0000

    v0.0.3

--- hg0428db/__init__.py
+++ hg0428db/__init__.py
@@ -1,3 +1,3 @@
 from hg0428db.update import update
 from hg0428db.database import DataBase
-VERSION = '0.0.2'
+VERSION = '0.0.3'
--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
 from distutils.core import setup
 setup(
-  name = 'hg0428db',         # How you named your package folder (MyLib)
-  packages = ['hg0428db'],   # Chose the same as "name"
-  version = '0.0.2',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A simple remote database system designed for replit terminal games.',   # Give a short description about your library
-  author = 'Hudson Gouge',                   # Type in your name
-  author_email = 'hudson.gouge@icloud.com',      # Type in your E-Mail
-  url = 'https://github.com/hg0428/hg0428db',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/hg0428/hg0428db/archive/refs/tags/v0.0.1.tar.gz',    # I explain this later on
+  name = 'hg0428db',  
+  packages = ['hg0428db'], 
+  version = '0.0.3',  
+  license='MIT', 
+  description = 'A simple remote database system designed for replit terminal games.', 
+  author = 'Hudson Gouge',        
+  author_email = 'hudson.gouge@icloud.com',     
+  url = 'https://github.com/hg0428/hg0428db',  
+  download_url = 'https://github.com/hg0428/hg0428db/archive/refs/tags/v0.0.2.tar.gz',
   keywords = ['Database', 'db', 'API'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
-          'warnings'
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
+    'Programming Language :: Python :: 3',  
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
